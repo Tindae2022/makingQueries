a8281c98bbbe3a48e5f4c830d00b1d59f6629919
Tindae2022 <alusinelavalie80@gmail.com>
2023-08-26 20:44:32 +0200

    Added the Blog models

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,14 @@ from django.db import models
 
 from datetime import date
 
+
+class Bolg(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def __str__(self):
+        return self.name
+
 class Author(models.Model):
     name = models.Charfield()
     email = models.EmailField()
